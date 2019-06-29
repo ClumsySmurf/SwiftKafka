3e2e2adbac7241742e3ce8c9b4fa3f0c778f8ee7
John Hamilton <clumsysmurf30@gmail.com>
2019-06-28 17:32:54 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,33 +1,37 @@
-// swift-tools-version:5.0
+/ swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "Dependencies",
+    name: "SwiftKafka",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Dependencies",
-            type: .static,
-            targets: ["Dependencies"]),
+            name: "SwiftKafka",
+            targets: ["SwiftKafka"]),
     ],
     dependencies: [
-      .package(url: "git@github.com:ClumsySmurf/SwiftKafka.git", .branch("master"))
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
-      .systemLibrary(name: "Crdkafka",
-                     path: "./Sources/Crdkafka",
-                     pkgConfig: "rdkafka",
-                     providers: [
-                          .brew(["librdkafka"]),
-                          .apt(["librdkafka-dev"])
-                     ]
-      ),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .systemLibrary(name: "Crdkafka",
+                       path: "./Sources/Crdkafka",
+                       pkgConfig: "rdkafka",
+                       providers: [
+                            .brew(["librdkafka"]),
+                            .apt(["librdkafka-dev"])
+                       ]
+        ),
         .target(
-            name: "Dependencies",
-            dependencies: ["SwiftKafka", "Crdkafka"])
-      ]
+            name: "SwiftKafka",
+            dependencies: ["Crdkafka", "Logging"]
+        ),
+        .testTarget(
+            name: "SwiftKafkaTests",
+            dependencies: ["SwiftKafka"]
+        ),
+    ]
 )
