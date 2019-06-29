17a5c5991990a2e5e8ba79207398dff943e622e5
John Hamilton <clumsysmurf30@gmail.com>
2019-06-28 17:37:48 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftKafka",
+            type: .static,
             targets: ["SwiftKafka"]),
     ],
     dependencies: [
@@ -18,7 +19,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .systemLibrary(name: "Crdkafka",
-                       path: "./Sources/Crdkafka",
                        pkgConfig: "rdkafka",
                        providers: [
                             .brew(["librdkafka"]),
